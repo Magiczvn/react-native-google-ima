82f78b7188fbe247cfc2511acc5934e7324949c5
Tam Nguyen <tam.nguyen@pops.vn>
2017-06-02 16:23:04 +0700

    Fixed wrong params

--- android/src/main/java/com/popsworldwide/RNReactNativeGoogleImaPackage.java
+++ android/src/main/java/com/popsworldwide/RNReactNativeGoogleImaPackage.java
@@ -13,8 +13,21 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.Callback;
 
 public class RNReactNativeGoogleImaPackage implements ReactPackage {
+    Callback onAdsStartedCallback, onAdsCompletedCallback, onAdsErrorCallback;
+
+    public RNReactNativeGoogleImaPackage(Callback onAdsStartedCallback, Callback onAdsCompletedCallback, Callback onAdsErrorCallback) {
+        this.onAdsStartedCallback = onAdsStartedCallback;
+        this.onAdsCompletedCallback = onAdsCompletedCallback;
+        this.onAdsErrorCallback = onAdsErrorCallback;
+    }
+
+    public RNReactNativeGoogleImaPackage() {
+      this(null, null, null);
+    }
+
+
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext, Callback onAdsStartedCallback, Callback onAdsCompletedCallback, Callback onAdsErrorCallback) {
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new RNReactNativeGoogleImaModule(reactContext, onAdsStartedCallback, onAdsCompletedCallback, onAdsErrorCallback));
     }
 
